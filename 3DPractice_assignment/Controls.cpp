af4322bc673577d8aa545a0d290c46944ebeadc4
HwangJuHyun <hyunhwang091@naver.com>
2024-11-29 20:49:40 +0900

    fist commit

---
+++ 3DPractice_assignment/Controls.cpp
@@ -0,0 +1,44 @@
+#include "Controls.h"
+#include "Level.h"
+#include <GLFW/glfw3.h>
+
+void Controls::keyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mods)
+{
+    //ESC - CLOSE WINDOW
+    if (action == GLFW_PRESS)
+        if (key == GLFW_KEY_ESCAPE)
+            glfwSetWindowShouldClose(pWindow, GL_TRUE);
+
+    //SPACE - RELOAD SHADER
+    if (action == GLFW_PRESS)
+        if (key == GLFW_KEY_SPACE)
+            Level::GetPtr()->ReloadShaderProgram();
+
+
+    //TODO: ADD CAMERA CONTROLS 
+    
+    if (action == GLFW_REPEAT)
+    {
+        if (key == GLFW_KEY_W)
+        {
+            Level::GetPtr()->RotateCamX(5.0f);
+        }
+        if (key == GLFW_KEY_A)
+        {
+            Level::GetPtr()->RotateCamY(5.0f);
+        }
+        if (key == GLFW_KEY_D)
+        {
+            Level::GetPtr()->RotateCamY(-5.0f);
+        }
+        if (key == GLFW_KEY_S)
+        {
+            Level::GetPtr()->RotateCamX(-5.0f);
+        }
+    }
+    //TODO: ADD/DECRESE SLICES
+    //TODO: TRIGGER WIREFRAME
+    //TODO: TRIGGER TEXTURE
+    //TODO: TRIGGER NORMALS RENDER
+    //TODO: TRIGGER NORMALS AVERAGE
+}
